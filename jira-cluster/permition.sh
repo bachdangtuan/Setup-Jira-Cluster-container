359af780ab345991bd2ad0a009fe3d20f0627837
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-05 08:39:10 +0700

    update dockerfile

--- jira-cluster/permition.sh
+++ jira-cluster/permition.sh
@@ -1,4 +1,4 @@
 chmod -R o+rwx jira-*
-chmod -R o+rwx jira-sharedata
-chown -R 2001:2001 jira-* 
-chown -R 2001:2001 jira-sharedata
+# chmod -R o+rwx jira-sharedata
+chown -R 2001:2001 jira-01
+# chown -R 2001:2001 jira-sharedata
